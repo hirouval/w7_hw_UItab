e46da69ed5522504e91e12e8a878d0b741d46a46
hirouval <ftisland1218@icloud.com>
2023-11-29 23:13:41 +0900

    htmlにis-activeを追加していたため削除しました

--- tab.js
+++ tab.js
@@ -3,19 +3,20 @@
     const $tab = $doc.getElementById('js-nav');
     const $nav = $tab.querySelectorAll('[data-nav]');
     const $content = $tab.querySelectorAll('[data-content]');
-    const ACTIVE_CLASS = 'is-active';
+    const ACTIVE_CLASS = 'is-active'; //変わらない定数は大文字で書く
     const navLen = $nav.length;
   
     // console.log('$contents' , $contents);
   
-    //初期化
+    //イニシャライズ（一番最初に動いてほしい命令）初期化
+    // 一番左のコンテンツの内容を表示させる（他は表示させない）
     const init = () => {
       $content[0].style.display = 'block';
     };
     init();
     //クリックしたら起こるイベント
     const handleClick = (e) => {
-      e.preventDefault();
+      e.preventDefault(); //リンクのイベントをなくす命令
   
       console.log('e!' , e);
       //クリックされたnavとそのdataを取得
@@ -30,13 +31,14 @@
         index++;
       }
   
-      console.log('$targetVal' , targetVal);
+      // console.log('$targetVal' , targetVal);
   
       //対象のコンテンツをアクティブ化する
       $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
       $nav[targetVal].classList.add(ACTIVE_CLASS);
     
     };
+    //全nav要素に対して関数を適用
     let index = 0;
     while(index < navLen){
       $nav[index].addEventListener('click' , (e) => handleClick(e));
